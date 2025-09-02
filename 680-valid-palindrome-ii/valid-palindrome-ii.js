4d5026a45a08ba83cfb44f7bfff8f80ff9a98282
Mithilesh Kumar Yadav <75425072+mky120799@users.noreply.github.com>
2025-09-02 17:26:54 +0530

    Time: 7 ms (59.67%) | Memory: 60.3 MB (83.09%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+   const isPal =(i,j) =>{
+    while(i < j){
+        if(s[i]!== s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+   };
+
+   let i=0; j = s.length-1;
+   while(i<j){
+    if(s[i]!== s[j]){
+        return isPal(i + 1, j) || isPal(i,j-1);
+        }
+        i++;
+        j--;
+   }
+   return true;
+};
